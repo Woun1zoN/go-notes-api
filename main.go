8c0396cf204dd0c981cd32da19645f972a687ca5
WountzoN <rantys.starblast228@gmail.com>
2026-02-17 20:17:50 +0300

    Added Middleware recovery

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"fmt"
 	"errors"
+	"runtime/debug"
 
 	"github.com/go-chi/chi"
 	"github.com/jackc/pgx/v5"
@@ -56,6 +57,23 @@ type PatchDTO struct {
     Content *string `json:"content" validate:"omitempty,min=1"`
 }
 
+// Middleware Recovery
+
+func RecoveryMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			stack := debug.Stack()
+
+			if err := recover(); err != nil {
+				log.Printf("PANIC: %v\n[%s] | {%s} | [IP:PORT - %s]\nStack:\n%s\n", err, r.Method, r.URL.String(), r.RemoteAddr, stack)
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 // Middleware Logger
 
 func Logger(next http.Handler) http.Handler {
@@ -77,7 +95,7 @@ func Logger(next http.Handler) http.Handler {
 	})
 }
 
-// Context Middleware
+// Middleware Context
 
 func ContextMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -305,6 +323,7 @@ func main() {
 
 	r := chi.NewRouter()
 
+	r.Use(RecoveryMiddleware)
 	r.Use(Logger)
 	r.Use(ContextMiddleware)
 
